67e7022e38e6c9b6fe0843b142e361bd9df001f8
OlegKozintsev <on.kozintsev@gmail.com>
2019-11-19 18:52:28 +0200

    adapt-1911

--- app/js/main.js
+++ app/js/main.js
@@ -104,7 +104,6 @@ var caseSlider = $('#case').lightSlider({
                 item:1,
                 slideMove:1,
                 enableTouch: true,
-                adaptiveHeight: true,
                 slideMargin:5
               }
         }
@@ -139,5 +138,85 @@ $('#caseRight').click(function(){
   })(jQuery);
 
 
-  });
+  (function($) {
+    $(function() {
+      $("ul.menu__list").on("mouseover", "li:not(.active)", function() {
+        $(this)
+          .addClass("active")
+          .siblings()
+          .removeClass("active")
+          .closest("div.tabs")
+          .find("div.tabs__content")
+          .removeClass("active")
+          .eq($(this).index())
+          .addClass("active");
+      });
+    });
+  })(jQuery);
 
+
+  (function($) {
+    $(function() {
+      $("#burgerLabel").on("click", function() {
+        
+        
+        if($("#burgerLabel").hasClass('active')) {
+            console.log('test');
+            $("#burgerLabel").removeClass('active');
+            $('.menu').removeClass("show");
+            $('.contact').removeClass("show");
+        }
+        else {
+            $("#burgerLabel").addClass('active');
+            $('.menu').addClass("show");
+            $('.contact').addClass("show");
+        }
+       
+      });
+    });
+  })(jQuery);
+            
+ // menu
+(function($) {  
+    $(document).on("mouseout", function(e){
+
+        var div = $("ul.menu__list > li"); // тут указываем ID элемента
+            if (!div.is(e.target) // если клик был не по нашему блоку
+                && div.has(e.target).length === 0) { // и не по его дочерним элементам
+                    div.removeClass('active'); // скрываем его
+                  
+            }
+    });
+})(jQuery);
+
+(function($) {  
+    $(document).on("click", function(e){
+
+        var div = $("#headerContact"); // тут указываем ID элемента
+            if (!div.is(e.target) // если клик был не по нашему блоку
+                && div.has(e.target).length === 0) { // и не по его дочерним элементам
+                    $("#burgerLabel").removeClass('active');
+            $('.menu').removeClass("show");
+            $('.contact').removeClass("show");
+            $("#burger").prop('checked', false);
+                  
+            }
+    });
+})(jQuery);
+
+function f_acc(){
+    //скрываем все кроме того, что должны открыть
+      $('.submenu').not($(this).next()).slideUp(200);
+    // открываем или скрываем блок под заголовком, по которому кликнули
+        $(this).next().slideToggle(400);
+    }
+    
+$('.menu__list > li > a').on('click', f_acc);
+
+
+
+});
+
+
+
+  
